58b922c00c599c98389429aee477d55f55d33221
Lucas Meireles <meireles31@msn.com>
2017-05-13 15:47:52 -0300

    Create page for visa card

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return jsonify({"message" : "It's alive"})
+
+@app.route('/visa')
+def visa():
+    return render_template('visa.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
